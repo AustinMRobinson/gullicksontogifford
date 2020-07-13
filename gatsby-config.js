4ef4e995df2c9fc4aa642eb8c29518502ca88318
Austin Robinson <austinrobinsondesign@gmail.com>
2020-07-12 21:36:14 -0500

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gullickson to Gifford`,
+        short_name: `G to G`,
+        description: `A bridal shower taking place for Piper Gullickson in August`,
+        lang: `en`,
+        display: `standalone`,
+        icon: `src/images/gtogfavicon.svg`,
+        start_url: `/`,
+        background_color: `#fffdf9`,
+        theme_color: `cf9f46`,
+      },
+    },
+    {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         custom: {
